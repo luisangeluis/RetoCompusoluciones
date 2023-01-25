51c913913fd97a0f4fe4e4cd3d39381ac9b3cf61
luisangeluis <65631946+luisangeluis@users.noreply.github.com>
2023-01-24 18:00:18 -0600

    Making form modal

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react';
+//Components
+import { useSelector } from 'react-redux';
+import FormModal from './modal/FormModal';
 
 const Home = () => {
-  
+  const isShowForm = useSelector((state) => state.showModal);
+  console.log(isShowForm);
   return (
     <section className="home flex-grow-1">
       <div className="container"></div>
+      {isShowForm && <FormModal />}
     </section>
   );
 };
--- src/components/modal/FormModal.jsx
+++ src/components/modal/FormModal.jsx
@@ -1,45 +1,53 @@
 const FormModal = () => {
   return (
-    <section className="form-modal">
-      <div className="container">
-        <form>
-          <div className="mb-3">
-            <label for="name" className="form-label">
-              Nombre
-            </label>
+    <section className="form-modal position-absolute top-50 start-50 translate-middle d-flex justify-content-center align-items-center w-100 h-100">
+      <div className="container bg-light rounded p-2 p-md-3 modal-container">
+        <form className=" modal-sm">
+          <div className="form-floating mb-3">
             <input
               type="text"
               className="form-control"
+              placeholder="name"
               id="name"
               aria-describedby="name"
             />
-            <div id="emailHelp" className="form-text">
-              We'll never share your email with anyone else.
-            </div>
+            <label htmlFor="name">Nombre</label>
           </div>
           <div className="mb-3">
-            <label for="fecha-constitucion" className="form-label">
-              Password
+            <label htmlFor="constitution-date" className="form-label">
+              Fecha de constitución
             </label>
             <input
-              type=""
+              type="date"
               className="form-control"
-              id="exampleInputPassword1"
+              id="constitution-date"
             />
           </div>
-          <div className="mb-3 form-check">
-            <input
-              type="checkbox"
-              className="form-check-input"
-              id="exampleCheck1"
-            />
-            <label className="form-check-label" for="exampleCheck1">
-              Check me out
+          <div className="mb-3">
+            <label htmlFor="" className="form-label" id="company-type">
+              Tipo de empresa
             </label>
+            <br />
+            <select
+              id="company-type"
+              className="select-company form-select-sm"
+              aria-label="Select your type of company"
+            >
+              <option defaultValue>Select your company</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
           </div>
-          <button type="submit" className="btn btn-primary">
-            Submit
-          </button>
+          <div class="form-floating mb-3">
+            <input
+              type="text"
+              className="form-control"
+              id="comentarios"
+              placeholder="Comentarios"
+            />
+            <label htmlFor="comentarios">Comentarios</label>
+          </div>
         </form>
       </div>
     </section>
